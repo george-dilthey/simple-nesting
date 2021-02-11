b8eb452b0c35360a4827a5aec056d375a7bf604b
George Dilthey <george.dilthey@gmail.com>
2021-02-11 17:31:54 -0500

    commit tree b60d9e972cf8a022a1c23e5c42e810220c955adc

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+     programmer_hash[:hopper] = {known_for: "COBOL", languages: ["COBOL", "FORTRAN"]}
 
 
 end
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:dennis_ritchie][:languages][0]   
 end
 
 def adding_matz
@@ -80,6 +86,13 @@ def adding_matz
         }
      }
 
+     programmer_hash[:yukihiro_matsumoto] = {
+         :known_for => "Ruby",
+         :languages => ["LISP", "C"]
+      }
+
+      return programmer_hash
+
     
 end
 
@@ -101,6 +114,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
      
 end
@@ -121,5 +136,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     programmer_hash
 
 end
